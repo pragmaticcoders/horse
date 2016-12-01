5f5c5fa6cd321f8384cbabb7d829b4185a7f0ade
Marcin Biernat <biern.m@gmail.com>
2016-12-01 11:45:20 +0100

    smart recommendations include users similarity

--- horse/recommendation/smart.py
+++ horse/recommendation/smart.py
@@ -18,13 +18,16 @@ class Relation:
 
 
 class SmartRecommendationService(RecommendationService):
+    base_potential = 2
+
     def __init__(self, user_repo, movie_repo):
         self.user_repo = user_repo
         self.movie_repo = movie_repo
 
-    def _get_base_movie_recommendations(self):
+    def _get_base_movie_recommendations(self, movies_to_skip):
         return [
             Recommendation(movie, 1) for movie in self.movie_repo.all()
+            if movie not in movies_to_skip
         ]
 
     def _get_related_users(self, user):
@@ -40,7 +43,32 @@ class SmartRecommendationService(RecommendationService):
 
         recur(user, 1)
 
+        for other_user in relations:
+            similarity = self._calculate_user_similarity_factor(
+                user, other_user)
+            relations[other_user] += relations[other_user] * similarity
+
         return relations
+
+    def _calculate_user_similarity_factor(self, user, other):
+        user_a_movies = set(user.get_liked_movies())
+        user_b_movies = set(other.get_liked_movies())
+
+        common_liked_movies_count = len(user_a_movies & user_b_movies)
+        max_common_movies_count = min(len(user_a_movies), len(user_b_movies))
+
+        if max_common_movies_count == 0:
+            # Don't make any assumptions if one of the users doesn't
+            # have anything rated
+            return 0
+
+        common_movies_fraction = (
+            common_liked_movies_count / max_common_movies_count)
+
+        potential = (max_common_movies_count / self.base_potential)
+        potential = max(min(potential, 2), 0.5)
+
+        return common_movies_fraction * potential
 
     def _increase(self, recommendations, movie, score):
         recommendation = [
@@ -49,11 +77,16 @@ class SmartRecommendationService(RecommendationService):
         recommendation.score += score
 
     def recommend(self, user):
-        recommendations = self._get_base_movie_recommendations()
+        movies_to_skip = user.get_liked_movies()
+
+        recommendations = self._get_base_movie_recommendations(movies_to_skip)
         user_relations = self._get_related_users(user)
 
         for related_user, relation_score in user_relations.items():
             for movie in related_user.get_liked_movies():
+                if movie in movies_to_skip:
+                    continue
+
                 self._increase(recommendations, movie, relation_score)
 
         recommendations = sorted(recommendations, key=lambda r: -r.score)
--- tests/integration/recommendation/test_smart_service.py
+++ tests/integration/recommendation/test_smart_service.py
@@ -15,6 +15,22 @@ def assert_recommendations(result, expected):
     assert readable(result) == readable(expected)
 
 
+def test_liked_movie_is_not_included(service, movies_repo):
+    user = User('root')
+
+    movie_a = Movie('a')
+    movie_b = Movie('b')
+
+    movies_repo.store(movie_a)
+    movies_repo.store(movie_b)
+
+    user.add_to_liked_movies(movie_a)
+
+    result = service.recommend(user)
+
+    assert_recommendations(result, [movie_b])
+
+
 def test_followed_users_movie_is_more_influential(service, movies_repo):
     user = User('root')
     other_user = User('other')
@@ -57,3 +73,31 @@ def test_nested_follows_are_more_influential(service, movies_repo):
     result = service.recommend(user)
 
     assert_recommendations(result, [movie_a, movie_b])
+
+
+def test_similar_users_are_more_influential(service, movies_repo):
+    movie_a = Movie('a')
+    movie_b = Movie('b')
+    movie_c = Movie('c')
+
+    movies_repo.store(movie_a)
+    movies_repo.store(movie_b)
+    movies_repo.store(movie_c)
+
+    user = User('root')
+    followed_user_1 = User('followed 1')
+    followed_user_2 = User('followed 2')
+
+    user.add_to_followed_users(followed_user_1)
+    user.add_to_followed_users(followed_user_2)
+
+    followed_user_1.add_to_liked_movies(movie_a)
+    followed_user_2.add_to_liked_movies(movie_b)
+    followed_user_2.add_to_liked_movies(movie_c)
+
+    # User shares a common movie with followed_user_2
+    user.add_to_liked_movies(movie_c)
+
+    result = service.recommend(user)
+
+    assert_recommendations(result, [movie_b, movie_a])
